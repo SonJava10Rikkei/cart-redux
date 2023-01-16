9178fecf75049f5f581403f7c160016a227202d8
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-16 18:22:26 +0700

    a

---
+++ cart_redux_t2/src/dummy/product.js
@@ -0,0 +1,10 @@
+export const product = [
+  { id: 1, name: "book1", price: 10000 },
+  { id: 2, name: "book2", price: 20000 },
+  { id: 3, name: "book3", price: 30000 },
+  { id: 4, name: "book4", price: 40000 },
+  { id: 5, name: "book5", price: 50000 },
+  { id: 6, name: "book6", price: 60000 },
+  { id: 7, name: "book7", price: 70000 },
+  { id: 8, name: "book8", price: 80000 },
+];
